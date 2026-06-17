7141d4fe994252803b0d9e0f763dbf4a5f0ecbe6
xinxing <xinxingaxx88@gmail.com>
2026-06-17 10:34:46 +0800

    肩袖再撕裂风险计算器 - 多中心循证模型

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'rotator-cuff-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
